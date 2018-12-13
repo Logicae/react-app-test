35c2475725ce8a96bc4466f6ff204507d02a40f7
Logicae <17463839+Logicae@users.noreply.github.com>
2018-12-13 13:27:36 -0500

    created card(s) and deleted extra file

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = () => {
+    return (
+        <div>
+            <img alt='robots' src='https://robohash.org/testing?200x200'/>
+            <div>
+                <h2>Robot</h2>
+                <p>robot@email.com</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
