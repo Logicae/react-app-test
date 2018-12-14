e647faebc19cb2ab4c6a8b7bb2af91ed5a107ba7
Logicae <17463839+Logicae@users.noreply.github.com>
2018-12-13 20:12:24 -0500

    destructed and added props

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Card = () => {
+const Card = ({name, id, email}) => {
     return (
         <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5" >
-            <img alt='robots' src='https://robohash.org/testing?200x200'/>
+            <img alt='robots' src={`https://robohash.org/${id}?200x200`}/>
             <div>
-                <h2>Robot</h2>
-                <p>robot@email.com</p>
+                <h2>{name}</h2>
+                <p>{email}</p>
             </div>
         </div>
     );
