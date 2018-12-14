4da4a19df5b332f5a59022c6018dfc3e5d856237
Logicae <17463839+Logicae@users.noreply.github.com>
2018-12-13 19:13:17 -0500

    added robots.js

---
+++ src/robots.js
@@ -0,0 +1,26 @@
+export const robots = [
+    {
+        id: 1, 
+        name: 'Bob Smith', 
+        username: "bobby",
+        email: "bobs@email.com"
+    },
+    {
+        id: 2, 
+        name: 'Sussy Smalls', 
+        username: "s",
+        email: "Suss@email.com"
+    },
+    {
+        id: 3, 
+        name: 'Bill Ios', 
+        username: "bill",
+        email: "bill@email.com"
+    },
+    {
+        id: 4, 
+        name: 'Bob Franks', 
+        username: "franks",
+        email: "frank@email.com"
+    }
+]
