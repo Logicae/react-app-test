07bb43d41b4fc100bd860104f044e40b0b99c726
Logicae <17463839+Logicae@users.noreply.github.com>
2018-12-17 11:49:51 -0500

    added search component and state in app component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardList from './CardList';
+import SearchBox from './SearchBox';
 import { robots } from './robots';
 
-const App = () => {
-    < CardList robots={robots} />
+ class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: robots, 
+            searchfield: ''
+        }
+    }
+
+    onSearchChange(event) {
+        console.log(event)
+    }
+
+    render() { 
+        return (
+            <div className='tc'>
+                <h1>RoboFriends</h1>
+                <SearchBox/>
+                <CardList robots={this.state.robots}/>
+            </div>
+        );
+    }
 }
+
+export default App;
